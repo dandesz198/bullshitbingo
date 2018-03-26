eec13673030358d68ff5e05b87853a43f24d29f2
Daniel <dandesz198@gmail.com>
2018-03-26 20:40:16 +0200

    Made the background colors darker

--- src/Home.js
+++ src/Home.js
@@ -23,7 +23,7 @@ export default class Home extends React.Component {
   render() {
     var bgColor = this.state.x.interpolate({
         inputRange: [1, 2, 3, 4, 5, 6],
-        outputRange: ['rgb(26, 188, 156)', 'rgb(46, 204, 113))', 'rgb(52, 152, 219)', 'rgb(155, 89, 182)', 'rgb(231, 76, 60)', 'rgb(230, 126, 34)']
+        outputRange: ['rgb(22, 160, 133)', 'rgb(39, 174, 96)', 'rgb(41, 128, 185)', 'rgb(142, 68, 173)', 'rgb(211, 84, 0)', 'rgb(44, 62, 80)']
     });
 
     return (
