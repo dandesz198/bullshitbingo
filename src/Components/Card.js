d8cd5c5facd0a33c3f5a22b4d7bbb45eab26a7ad
Hajdu Péter <hajdupetke@gmail.com>
2018-03-26 21:10:25 +0200

    Initial Card Setup

---
+++ src/Components/Card.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+class Card extends Component {
+    render() {
+        return(
+            <View>
+                <Text>Hello</Text>
+            </View>
+        )
+    }
+}
+
+const style = StyleSheet.create({
+    boxStyle: {
+
+    },
+    textStyle: {
+
+    },
+    
+})
