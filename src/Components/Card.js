77f95391709e4554f445df66301c44b63de39252
Hajdu Péter <hajdupetke@gmail.com>
2018-03-29 15:38:54 +0200

    Finished Card.js

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,15 +1,29 @@
 import React, { Component } from 'react'
-import { View, Text, StyleSheet,Button } from 'react-native'
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
 
 class Card extends Component {
     constructor(props) {
         super(props)
+        this.state = {
+            text: "Vote!",
+            pressed: false,
+        }
     }
     render() {
         return(
             <View style={style.boxStyle}>
-                <Text>{this.props.cardText}</Text>
-                <Text>{this.props.voteNumber}</Text>
+                <View style={style.textBoxStyle}>
+                    <Text style={style.roomNameText}>{this.props.roomName}</Text>
+                    <Text style={style.nameText}>{this.props.voteName}</Text>
+                </View>
+                <View style={style.buttonBoxStyle}>
+                    <TouchableOpacity style={style.buttonStyle} onPress={() => this.setState({text: 'Voted'})}>
+                        <Text style={style.buttonText}>{this.state.text}</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={[style.secondButton, this.state.pressed ? style.buttonStylePressed :style.buttonStyle]} onPress={() => this.setState({pressed: true})}>
+                        <Text style={style.buttonText}>Bingo</Text>
+                    </TouchableOpacity>
+                </View>
             </View>
         )
     }
@@ -17,12 +31,57 @@ class Card extends Component {
 
 const style = StyleSheet.create({
     boxStyle: {
-
+        shadowOffset: { width: 10, height: 10 },
+        shadowColor: 'black',
+        shadowOpacity: 1,
+        elevation: 3,
+        // background color must be set
+        backgroundColor : "#0000", // invisible color
+        margin: 10,
+        zIndex:999,
+        display: 'flex',
+    }, 
+    roomNameText: {
+        color: '#95a5a6'
     },
-    cardText: {
-
+    nameText: {
+        color: '#555',
+        fontSize: 30,
     },
-
+    textBoxStyle: {
+        padding: 10,
+        backgroundColor: '#ecf0f1'
+    },
+    buttonBoxStyle: {
+        backgroundColor: '#bdc3c7',
+        display: "flex",
+        alignItems: 'flex-start',
+        flexDirection: 'row',
+        padding: 10,
+    },
+    buttonStyle: {
+        backgroundColor: "#555",
+        paddingTop: 5,
+        paddingBottom: 5,
+        paddingLeft: 30,
+        paddingRight: 30,
+        borderRadius: 5
+    },
+    buttonStylePressed: {
+        backgroundColor: "green",
+        paddingTop: 5,
+        paddingBottom: 5,
+        paddingLeft: 30,
+        paddingRight: 30,
+        borderRadius: 5
+    },
+    secondButton: {
+        marginLeft: 10,
+    },
+    buttonText: {
+        color: "#fff",
+        fontWeight: "bold",
+    }
 })
 
 export default Card;
